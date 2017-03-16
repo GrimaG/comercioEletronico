1aa08ca9dee411af1a75467abd4bd1bda779ff4a
GrimaG <guh.grimaldi@hotmail.com>
2017-03-16 10:19:17 -0300

    comercio eletronico js

---
+++ config/custom-express.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var cors = require('cors');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+
+module.exports = function () {
+  var app = express();
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  consign()
+    .include('controllers').then('persistencia')
+    .into(app);
+
+  return app;
+}
---
+++ controllers/pedidos.js
@@ -0,0 +1,99 @@
+module.exports = function (app) {
+  var fs = require('fs');
+
+  app.get('/pedidos', function (req, res) {
+
+    var path = require('path');
+
+    var filePath = path.join(__dirname, 'pedidos.txt');
+
+    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+      if (!err) {
+        res.json(data);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+  app.post('/pedidos', function (req, res) {
+
+    var path = require('path');
+
+    var filePath = path.join(__dirname, 'pedidos.txt');
+
+    var pedido = req.body;
+    console.log(pedido);
+    var json = JSON.stringify(pedido);
+    var obj = [];
+    console.log(json);
+    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("data " + data.length);
+        if (data.length == 2) {
+          fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+            if (err) {
+              console.log(err);
+            } else {
+              var ress = data.replace(']', json + ']');
+              console.log('nova data :' + ress);
+              fs.writeFile(filePath, ress, 'utf8', function (err) { }); // write it back 
+            }
+          });
+        } else {
+          fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+            if (err) {
+              console.log(err);
+            } else {
+
+              var lasti = data.lastIndexOf(']');
+              var ress = data.substring(0, lasti) + ',' + json + ']';
+
+              //  = data.replace(']', ',' + json + ']');
+              console.log('nova data :' + ress);
+
+              fs.writeFile(filePath, ress, 'utf8', function (err) { }); // write it back 
+            }
+          });
+        }
+      }
+    });
+  });
+
+
+  app.get('/pedidos/deleta', function (req, res) {
+
+    var path = require('path');
+
+    var filePath = path.join(__dirname, 'pedidos.txt');
+
+    var pedido = req.body;
+    console.log(pedido);
+    var json = JSON.stringify(pedido);
+    var obj = [];
+    console.log(json);
+    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("data " + data.length);
+        fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+
+            var lasti = data.lastIndexOf(']');
+            var ress = data.substring(0, lasti) + ',' + json + ']';
+            console.log('nova data :' + ress);
+
+            fs.writeFile(filePath, `[]`, 'utf8', function (err) { }); // write it back 
+            res.json('');
+          }
+        });
+
+      }
+    });
+  });
+
+}
